651ce5779fa33a1e3d08f13f844291cccc82cdba
Michael Noon <m.noon83@yahoo.com>
2021-09-13 17:31:11 -0700

    code

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const {join} = require('path')
 const app = express ()
 
-app.get('/test', (req,res)=>{
-  res.send('<h6>im batman</h6>')
+app.get('/', (req,res)=>{
+  res.sendFile(join(__dirname,'index.html'))
 })
+
+
+// app.get('/test', (req,res)=>{
+//   res.send('<h6>im batman</h6>')
+// })
 
 app.listen(3000)
 
